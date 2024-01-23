42adc8d034a76e0586c3dc0515586e6d9808282c
teklingl <teklingl@gmail.com>
2024-01-23 14:32:31 -0500

    Starter file(s).

---
+++ homework06/RecurseLoopCounting.java
@@ -0,0 +1,30 @@
+// CST 283 Homework 6
+
+public class RecurseLoopCounting
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println(sumEvenNumbers(1000));
+        System.out.println(sumEvenNumbersRecurse(1000));
+    }
+
+    // Write Java method that sums all all positive even numbers between 2 and n iteratively.
+    public static int sumEvenNumbers(int n)
+    {
+        int sum = 0;
+
+
+        return sum;
+    }
+
+    // Write Java method that sums all all positive even numbers between 2 and n iteratively.
+    // Preconditions:  n is even and n > 2
+    public static int sumEvenNumbersRecurse(int n)
+    {
+        int sum = 0;
+
+
+        return sum;
+    }
+}
